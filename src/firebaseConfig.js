37b4485321adeda43adf6c4718fa11a21de5c7f4
moisesnks <mleiva@utem.cl>
2024-04-18 11:55:29 -0400

    :duck: 273832 times

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,9 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
-
-// Inicializa Firebase con la configuración
+import firebaseConfig from "../credentials.json";
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
